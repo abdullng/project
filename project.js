28f18e3b8b6dc9ef03f12de1d30dbe88dc8c5de5
abdullng <124679489+abdullng@users.noreply.github.com>
2023-02-07 12:22:37 +0100

    Update project.js

--- project.js
+++ project.js
@@ -4,9 +4,9 @@ var imageURLs = [
      , "https://helpdesk.qixbit.com/images/desk-top-3.png"
   ];
   function getImageTag1() {
-    var img = '<img src=\"';
+    var img = '<img src="';
     var randomIndex = Math.floor(Math.random() * imageURLs.length);
     img += imageURLs[randomIndex];
-    img += '\" alt=\"Qixbit Trdading App\"/>';
+    img += '\" alt="Qixbit Trdading App\"/>';
     return img;
   }
